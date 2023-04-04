1e8aca951253b3332bb037e574f92373d5a5266f
Tcarters <cartersedmund@gmail.com>
2023-04-04 22:23:18 +0000

    Initial commit

---
+++ test/aws-cdk-infra-elastic_beanstalk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsCdkInfraElasticBeanstalk from '../lib/aws-cdk-infra-elastic_beanstalk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-cdk-infra-elastic_beanstalk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsCdkInfraElasticBeanstalk.AwsCdkInfraElasticBeanstalkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
